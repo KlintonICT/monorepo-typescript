53072e39061152ced81433cf507d3e5f2890516b
KlintonICT <chhunklinton91@gmail.com>
2023-09-27 21:41:52 +0700

    remove build folder

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ui_1 = require("@kt-package/ui");
-const utils_1 = require("@kt-package/utils");
-console.log((0, utils_1.isEven)(4));
-(0, ui_1.FooComponent)();
